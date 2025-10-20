5aad8b6fd4fd9f544124beab56eb08cb8dd4780e
aiden123317 <aiden123317@mcpsva.org>
2025-10-20 14:32:22 -0400

    Add files via upload

---
+++ Interview.py
@@ -0,0 +1,28 @@
+"""
+Filename: interview.py
+Author: <Myers, Aiden>
+Created 09/11/2025
+Instructor: Holtslander
+"""
+print("Hello, welcome to the job interview for Osbourn High School.")
+
+name = input("First, I'll need to know your name.\n")
+print(name,"? That's an interesting name, now let's get into the questions.")
+
+strength = input("What would you say is your greatest strength?\n")
+print(strength,"? That's good to know.")
+
+weakness = input("Now what is something you may struggle to do?\n")
+print(weakness,"? That's okay, we can work on that later.")
+
+reason = input("Why do you want to work here?\n")
+print(reason,"? Very interesting, we'll be sure to keep that in mind.")
+
+teamwork = input("Describe how productive you are in group settings.\n")
+print(teamwork,"? That's something we'll keep noted.")
+
+experience = input("How experienced are you in this field of work?\n")
+print(experience,"? Okay, that's good to know.")
+
+print("That was the last question! I will pass this onto someone to review and you will be notified about the job soon!")
+print("Thank you for using this program to help make the interview process easier.")
